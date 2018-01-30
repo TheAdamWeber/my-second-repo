e50204519cd536f4b9aea906dc01a7adf7e184c5
ajweber <ajweber@umich.edu>
2018-01-30 09:49:57 -0500

    Updated info

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 print('My name is Adam')
 print('I was killed and hatched in November')
 print('My favorite food is steak')
+print('I like a lot of music')
+print('I don\'t known my astrological sign')
 print('My birthday is september 30th')
 print('My name is ALSO ADAM')
 
